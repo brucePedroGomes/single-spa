c0bdd70b2fe6da7ac5a5b471a631d0376fa15e0a
pedro gomes <pedroleinar@hotmail.com>
2021-10-21 09:45:53 -0300

    add mfe

--- src/rpg-root-config.ts
+++ src/rpg-root-config.ts
@@ -6,14 +6,38 @@ registerApplication({
     System.import(
       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
     ),
-  activeWhen: ["/"],
+  activeWhen: (location) => location.pathname === "/",
+});
+
+registerApplication({
+  name: "@rpg/react-single",
+  app: () => System.import("@rpg/react-single"),
+  activeWhen: (location) => location.pathname === "/react-single",
+});
+
+registerApplication({
+  name: "@rpg/react-multiples",
+  app: () => System.import("@rpg/react-multiples"),
+  activeWhen: ["/react-multiples"],
 });
 
 // registerApplication({
-//   name: "@rpg/navbar",
-//   app: () => System.import("@rpg/navbar"),
-//   activeWhen: ["/"]
+//   name: "@rpg/react-parcel",
+//   app: () => System.import("@rpg/react-parcel"),
+//   activeWhen: (location) => location.pathname === "/react-parcel",
 // });
+
+registerApplication({
+  name: "@rpg/react-parcel-bro",
+  app: () => System.import("@rpg/react-parcel-bro"),
+  activeWhen: (location) => location.pathname === "/react-parcel-bro",
+});
+
+registerApplication({
+  name: "@rpg/react-lazy",
+  app: () => System.import("@rpg/react-lazy"),
+  activeWhen: ["/react-lazy"],
+});
 
 start({
   urlRerouteOnly: true,
